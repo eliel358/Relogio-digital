7d98ec5c8bdd5b7e6b32550c4418c79fe5791e71
eliel358 <92613589+eliel358@users.noreply.github.com>
2023-07-06 00:51:32 -0300

    update js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ function display() {
     date = new Date();
     hour = date.getHours();
     minute = date.getMinutes()
-    second = date.getSeconds()  
+    second = date.getSeconds()
+    console.log(hour+":"+minute+":"+second)    
     document.getElementById("horas").innerHTML = hour+":"+minute+":"+second
 }
 setInterval(display,10)
