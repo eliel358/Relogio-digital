8af5165b57310a0fb0b27fc401a569d7ad4f3be6
eliel358 <92613589+eliel358@users.noreply.github.com>
2023-07-26 08:23:14 -0300

    Deletando arquivos inuteis ou desnecessários

--- script.js
+++
@@ -1,9 +0,0 @@
-function display() {
-    date = new Date();
-    hour = date.getHours();
-    minute = date.getMinutes()
-    second = date.getSeconds()
-    console.log(hour+":"+minute+":"+second)    
-    document.getElementById("horas").innerHTML = hour+":"+minute+":"+second
-}
-setInterval(display,10)
